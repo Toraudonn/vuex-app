7829b367c5e0c09a650f0470f8f518ac9fdac525
haruyaishikawa <toraudonn@gmail.com>
2016-08-31 04:40:13 +0900

    Add files for vuex to work correctly

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: __dirname,
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      }
+    ]
+  },
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+},
+  "scripts": {
+      "dev": "webpack-dev-server --inline --hot",
+      "build": "webpack -p"
+  }
+}
